dc853ddd80558e72aa9cc1de0827d6fefa1fbf85
Darwin <accfordarwin@gmail.com>
2024-11-21 18:41:31 -0500

    First upload

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#[macro_use] extern crate rocket;
+use rocket::{
+    fs::{FileServer,NamedFile},
+    http::{Status},
+    Request,
+
+};
+#[get("/home")]
+async fn home() -> Option<NamedFile> {
+    let file: &str = "src/pages/home.html";
+    NamedFile::open(file).await.ok()
+}
+
+// Routes
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    let file: &str = "src/pages/index.html";
+    NamedFile::open(file).await.ok()
+}
+
+#[get("/404")]
+async fn four_o_four() -> Option<NamedFile> {
+    let file: &str = "src/pages/404.html";
+    NamedFile::open(file).await.ok()
+
+}
+
+
+
+
+#[catch(404)]
+async fn not_found() -> Option<NamedFile> {
+    let file: &str = "pages/404.html";
+    NamedFile::open(file).await.ok()
+
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .register("/", catchers![not_found])
+        .mount("/", routes![index, four_o_four, home])
+        .mount("/public", FileServer::from("src/static"))
+}
