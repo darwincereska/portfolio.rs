c57d7bfa20cf2ecfdd52a2acf283ccb6f20b9e23
Darwin <accfordarwin@gmail.com>
2024-11-22 02:29:09 -0500

    added contact with discord webhook

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use rocket::{
     fairing::AdHoc, fs::{FileServer, NamedFile}, http::Header, response::Response
 };
+use rocket::form::Form;
+use serde_json::json;
 
 #[rocket::async_trait]
 pub trait Compression {
@@ -14,6 +16,13 @@ async fn home() -> Option<NamedFile> {
     NamedFile::open(file).await.ok()
 }
 
+#[get("/contact")]
+async fn contact() -> Option<NamedFile> {
+    let file: &str = "src/pages/contact.html";
+    NamedFile::open(file).await.ok()
+}
+
+
 #[get("/")]
 async fn index() -> Option<NamedFile> {
     let file: &str = "src/pages/index.html";
@@ -25,6 +34,56 @@ async fn about() -> Option<NamedFile> {
     let file: &str = "src/pages/about.html";
     NamedFile::open(file).await.ok()
 }
+
+
+
+use reqwest::Client;
+
+#[derive(FromForm)]
+struct ContactForm {
+    name: String,
+    email: String,
+    message: String,
+}
+
+#[post("/contact", data = "<form>")]
+async fn contact_form(form: Form<ContactForm>) -> &'static str {
+    const WEBHOOK_URL: &str = "https://discord.com/api/webhooks/1309413812390199336/DmaOILGLgu8Z0RXGjhhwu5mYxFcl-L4jNtvFaINrrsogzWZjRRCQ2neAVyISu12JKgoD";
+    
+    let client = Client::new();
+    
+    let message = json!({
+        "embeds": [{
+            "title": "@everyone New Contact Form Submission",
+            "color": 16711935, // #FF00FF in decimal
+            "fields": [
+                {
+                    "name": "Name",
+                    "value": form.name,
+                    "inline": true
+                },
+                {
+                    "name": "Email",
+                    "value": form.email,
+                    "inline": true
+                },
+                {
+                    "name": "Message",
+                    "value": form.message
+                }
+            ]
+        }]
+    });
+
+    match client.post(WEBHOOK_URL)
+        .json(&message)
+        .send()
+        .await {
+            Ok(_) => "Message sent successfully!",
+            Err(_) => "Failed to send message."
+        }
+}
+
 
 #[get("/404")]
 async fn four_o_four() -> Option<NamedFile> {
@@ -51,6 +110,7 @@ fn rocket() -> _ {
             })
         }))
         .register("/", catchers![not_found])
-        .mount("/", routes![index, four_o_four, home, about])
+        .mount("/", routes![index, four_o_four, home, about, contact])
         .mount("/public", FileServer::from("src/static"))
+        .mount("/api", routes![contact_form])
 }
